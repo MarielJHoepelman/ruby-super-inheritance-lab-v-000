e1dd0e9f3771c4bdb19c18d1ea48f09d68f09066
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-12 16:36:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,8 @@ class Student
   def hello
     puts "Hey there! I'm so excited to learn stuff."
   end
-  
-  def self.raise_hand
+
+  def raise_hand
     10 times do
       puts "Pick me!"
     end
